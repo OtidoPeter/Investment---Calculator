3e46a2ffa434ecae9d3fc3fdf7892d07d565a09e
Otido Peter <peterotido@gmail.com>
2025-07-23 09:58:00 +0300

    writing to files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/investmentcalculator
+
+go 1.24.3
---
+++ investment_calculator.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+const inflationRate = 2.5
+
+const calculatedFutureValuesFile = "future.txt"
+
+func writeFloatToFile(fileName string, fv float64, rfv float64) {
+	valueText := fmt.Sprintf("Future Value: %.1f\nReal Future Value: %.1f\n", fv, rfv)
+	os.WriteFile(fileName, []byte(valueText), 0644)
+}
+
+func main() {
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
+
+	// fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
+	fmt.Scan(&investmentAmount) // Gets the user input
+
+	// fmt.Print("Investment horizon: ")
+	outputText("Investment horizon: ")
+	fmt.Scan(&years)
+
+	// fmt.Print("Expected return rate: ")
+	outputText("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	formattedFV := fmt.Sprintf("Future value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future value (adjusted for inflation: %.1f\n", futureRealValue)
+	// Outputs information
+	// fmt.Println("Future value:", futureValue)
+	// fmt.Printf(`Future value: %.1f
+
+	// Future value (adjusted for inflation): %.1f`, futureValue, futureRealValue)
+	// fmt.Println("Future value (adjusted for inflation):", futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	// return investmentAmount * math.Pow(1+expectedReturnRate/100, years), futureValue / math.Pow(1+inflationRate/100, years)
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	writeFloatToFile(calculatedFutureValuesFile, fv, rfv)
+	return fv, rfv
+	// return
+}
